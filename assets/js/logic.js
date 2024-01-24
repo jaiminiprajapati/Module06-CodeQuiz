ed249c0f52ce477ebf8647e1cf91d143323f60e1
jaiminiprajapati <jaiminihp397@gmail.com>
2024-01-24 09:46:03 +0000

    References to the HTML elements fetched and stored in logic.js file

---
+++ assets/js/logic.js
@@ -0,0 +1,14 @@
+// Get references to various HTML elements
+const startButton = document.getElementById("start");
+const startScreen = document.getElementById("start-screen");
+const timerElement = document.getElementById("time");
+const questionsContainer = document.getElementById("questions");
+const questionsTitle = document.getElementById("question-title");
+const choicesContainer = document.getElementById("choices");
+const endScreen = document.getElementById("end-screen");
+const finalScore = document.getElementById("final-score");
+const initials = document.getElementById("initials");
+const submitButton = document.getElementById("submit");
+const feedbackElement = document.getElementById("feedback");
+const correctSound = new Audio("./assets/sfx/correct.wav");
+const incorrectSound = new Audio("./assets/sfx/incorrect.wav");
