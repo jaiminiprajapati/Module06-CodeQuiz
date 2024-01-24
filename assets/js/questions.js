f652d845ca10077b0b26cfacfaa591496609904e
jaiminiprajapati <jaiminihp397@gmail.com>
2024-01-24 09:43:14 +0000

    Array of questions with options and answer added to the questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,54 @@
+// An array of objects containing questions, answer options, and correct answers
+
+const questions = [
+    {
+      question: "Javascript is an _______ language.",
+      options: ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+      correctAnswer: "Object-Oriented",
+    },
+    {
+      question: "Which of the following keywords is used to define a variable in Javascript?",
+      options: ["var", "let", "Both A and B", "None of the above"],
+      correctAnswer: "Both A and B",
+    },
+    {
+        question:"Which of the following methods is used to access HTML elements using Javascript?",
+        options:["getElementById()", "getElementsByClassName", "Both A and B", "None of the above"],
+        correctAnswer: "Both A and B",
+    },
+    {
+        question:"How can a datatype be declared to be a constant type?",
+        options:["const", "let", "var", "constant"],
+        correctAnswer: "const",
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        options: ["<javascript>", "<js>", "<script>", "<scripting>"],
+        correctAnswer: "script",
+    },
+    {
+        question:"JavaScript code can be written in ____.",
+        options: ["JavaScript file (.js file)", "HTML document directly", "Both A and B", "In style sheets (.css file)"],
+        correctAnswer: "Both A and B",
+    },
+    {
+        question: "JavaScript ignores?",
+        options: ["newlines", "tabs", "spaces", "All of the above"],
+        correctAnswer: "All of the above",
+    },
+    {
+        question: "Which property is used to define the HTML content to an HTML element with a specific id?",
+        options: ["innerText","innerContent","elementText","innerHTML"],
+        correctAnswer: "innerHTML",
+    },
+    {
+        question: "Which JavaScript method is used to write HTML output?",
+        options: ["document.write()", "response.write()", "write()", "document.output()"],
+        correctAnswer: "document.write()",
+    },
+    {
+        question: "In JavaScript, single line comment begins with ___.",
+        options: ["#", "$", "//", "/*"],
+        correctAnswer: "//",
+    },
+  ];
